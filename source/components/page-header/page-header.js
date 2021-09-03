f726928b3e91aa2d20f51e0ab9cad7dd16270391
dmkrupin <dev.dmkrupin@gmail.com>
2021-09-03 22:06:33 +0300

    Закончил верстку элементов шапки на всех вьюпортах

--- source/components/page-header/page-header.js
+++ source/components/page-header/page-header.js
@@ -1,15 +1,17 @@
-let menuToggle = document.querySelector('.page-header__menu-toggle');
-let menu = document.querySelector('.page-header__nav');
+let menuToggle = document.querySelector('.nav__toggle');
+let menu = document.querySelector('.nav__list');
+let pageHeader = document.querySelector('.page-header');
 
-menuToggle.classList.remove('page-header__menu-toggle--nojs');
-menu.classList.remove('page-header__nav--nojs');
+menuToggle.classList.remove('nav__toggle--nojs');
+menu.classList.remove('nav__list--nojs');
+pageHeader.classList.remove('page-header--nojs');
 
 menuToggle.addEventListener('click', function() {
-  if (menu.classList.contains('page-header__nav--closed')) {
-    menu.classList.remove('page-header__nav--closed');
-    menu.classList.add('page-header__nav--opened');
+  if (menu.classList.contains('nav__list--closed')) {
+    menu.classList.remove('nav__list--closed');
+    menu.classList.add('nav__list--opened');
   } else {
-    menu.classList.add('page-header__nav--closed');
-    menu.classList.remove('page-header__nav--opened');
+    menu.classList.add('nav__list--closed');
+    menu.classList.remove('nav__list--opened');
   }
 });
